3b819c48e7d4724f0e21c4645335aa0b289ad06a
Florian Fesseler <florian.fesseler@gmail.com>
2012-05-03 00:59:22 +0200

    added exercise 6

--- lesson3_parsing/test/allExercises.js
+++ lesson3_parsing/test/allExercises.js
@@ -82,3 +82,53 @@ describe('Scheme Syntax', function(){
 	    })
 	})
 })
+
+describe('Write a Scheem Parser', function(){
+	var parser;
+	before(function () {
+		grammar = fs.readFileSync("src/exercise5.pegjs").toString();
+		parser = wrapExceptions(PEG.buildParser(grammar).parse);  
+	});
+	describe('#lowercase words separated by space', function(){
+		it('should not parse empty string', function() {
+	    	expect(parser("")).to.be(undefined);
+	    }),
+	    it('should parse "atom"', function() {
+	    	expect(parser('atom')).to.eql('atom');
+	    }),
+	    it('should parse "+"', function() {
+	    	expect(parser('+')).to.eql('+');
+	    }),
+	    it('should parse "(+ x 3)"', function() {
+	    	expect(parser('(+ x 3)')).to.eql(['+', 'x', '3']);
+	    }),
+	    it('should parse "(+ 1 (f x 3 y))"', function() {
+	    	expect(parser("(+ 1 (f x 3 y))")).to.eql(["+", "1", ["f", "x", "3", "y"]]);
+	    })
+	})
+})
+
+describe('Parsing arithmetic in PEGs', function(){
+	var parser;
+	before(function () {
+		grammar = fs.readFileSync("src/exercise6.pegjs").toString();
+		parser = wrapExceptions(PEG.buildParser(grammar).parse);  
+	});
+	describe('#comma" infix operator that is lower precedence than addition', function(){
+		it('should parse 1+2', function() {
+	    	expect(parser("1+2")).to.eql({tag:"+", left:1, right:2});
+	    }),
+	    it('should parse 1+2*3', function() {
+	    	expect(parser("1+2*3")).to.eql({tag:"+", left:1, right:{tag:"*", left:2, right:3}});
+	    }),
+	    it('should parse 1,2', function() {
+	    	expect(parser("1,2")).to.eql({tag:",", left:1, right:2});
+	    }),
+	    it('should parse 1,2+3', function() {
+	    	expect(parser("1,2+3")).to.eql({tag:",", left:1, right:{tag:"+", left:2, right:3}});
+	    }),
+	    it('should parse 1*2,3', function() {
+	    	expect(parser("1*2,3")).to.eql({tag:",", left:{tag:"*", left:1, right:2}, right:3});
+	    })
+	})
+})
